5c625718661c1f31da424808a37fa86ddafbc83d
Jaehyun Song <tintstyle@gmail.com>
2024-09-02 11:01:59 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -2,5 +2,3 @@ install.packages("dagitty",  dependencies = TRUE, repos = "https://cran.rstudio.
 install.packages("rdd",      dependencies = TRUE, repos = "https://cran.rstudio.com/")
 install.packages("rdrobust", dependencies = TRUE, repos = "https://cran.rstudio.com/")
 install.packages("gsynth",   dependencies = TRUE, repos = "https://cran.rstudio.com/")
-
-install.packages("tinytable", repos = "https://vincentarelbundock.r-universe.dev")
