7737e00aa5801c72e90c287add24234b08337727
Jaehyun Song <tintstyle@gmail.com>
2025-08-05 16:20:49 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -1,3 +1,3 @@
-install.packages("rdd",      dependencies = TRUE, repos = "https://cran.rstudio.com/")
-install.packages("rdrobust", dependencies = TRUE, repos = "https://cran.rstudio.com/")
-install.packages("gsynth",   dependencies = TRUE, repos = "https://cran.rstudio.com/")
+install.packages("rdrobust",  dependencies = TRUE, repos = "https://cran.rstudio.com/")
+install.packages("gsynth",    dependencies = TRUE, repos = "https://cran.rstudio.com/")
+install.packages("interplot", dependencies = TRUE, repos = "https://cran.rstudio.com/")
