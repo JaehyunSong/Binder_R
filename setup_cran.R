5656d90d3ccc3a7a577dc9e4f33ac3ab175b271e
Jaehyun Song <tintstyle@gmail.com>
2025-08-05 16:41:54 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -1,5 +1,3 @@
-install.packages("https://cran.r-project.org/src/contrib/Archive/rdd/rdd_0.57.tar.gz")
-
 install.packages("rdrobust",  dependencies = TRUE, repos = "https://cran.rstudio.com/")
 install.packages("gsynth",    dependencies = TRUE, repos = "https://cran.rstudio.com/")
 install.packages("interplot", dependencies = TRUE, repos = "https://cran.rstudio.com/")
