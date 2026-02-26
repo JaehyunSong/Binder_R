5dc4fde5ba4f6a6ecffca3ef1564e35ac1ac9c44
Jaehyun Song <tintstyle@gmail.com>
2026-02-26 21:14:49 +0900

    Delete setup_cran.R

--- setup_cran.R
+++
@@ -1,5 +0,0 @@
-options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
-options(pkgType = "source")
-options(install.packages.compile.from.source = "never")
-
-install.packages(c("rdrobust", "gsynth", "interplot", "jpmesh"))
