6181d14006e321e9c1fc965556a9b7c6aa2b9690
Jaehyun Song <tintstyle@gmail.com>
2026-02-26 19:13:32 +0900

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,3 +1,7 @@
+options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
+options(pkgType = "source")
+options(install.packages.compile.from.source = "never")
+
 install.packages("rdd_0.57.tar.gz", type = "source", repos = NULL)
 
 print("全パッケージをアップデートする。\n")
