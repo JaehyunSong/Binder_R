649c8f956cf77eed912da166e8b1c0318bec9c64
Jaehyun Song <tintstyle@gmail.com>
2026-02-26 21:14:41 +0900

    Delete setup_gh.R

--- setup_gh.R
+++
@@ -1,6 +0,0 @@
-options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
-options(pkgType = "source")
-options(install.packages.compile.from.source = "never")
-
-remotes::install_github("uribo/jpndistrict", upgrade = "never")
-remotes::install_github("ropensci/rnaturalearthhires", upgrade = "never")
