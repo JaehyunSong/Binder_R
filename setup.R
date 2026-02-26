d3fcc46a9170658555ef49ecabf73f687458ea83
Jaehyun Song <tintstyle@gmail.com>
2026-02-26 19:16:37 +0900

    Update setup.R

--- setup.R
+++ setup.R
@@ -4,5 +4,5 @@ options(install.packages.compile.from.source = "never")
 
 install.packages("rdd_0.57.tar.gz", type = "source", repos = NULL)
 
-print("全パッケージをアップデートする。\n")
+print("全パッケージをアップデートする。")
 update.packages(ask = FALSE, checkBuilt = TRUE)
