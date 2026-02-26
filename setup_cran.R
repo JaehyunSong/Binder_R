4674b1a040f5c2339b746280bd2e363f72c8be8a
Jaehyun Song <tintstyle@gmail.com>
2026-02-26 19:13:05 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -1,3 +1,7 @@
+options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
+options(pkgType = "source")
+options(install.packages.compile.from.source = "never")
+
 install.packages("rdrobust",  dependencies = TRUE, repos = "https://cran.rstudio.com/")
 install.packages("gsynth",    dependencies = TRUE, repos = "https://cran.rstudio.com/")
 install.packages("interplot", dependencies = TRUE, repos = "https://cran.rstudio.com/")
