8d5905dda02afcb98145f41106b5d7d8fad3fb45
Jaehyun Song <tintstyle@gmail.com>
2026-02-26 19:13:19 +0900

    Update setup_gh.R

--- setup_gh.R
+++ setup_gh.R
@@ -1,2 +1,6 @@
+options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
+options(pkgType = "source")
+options(install.packages.compile.from.source = "never")
+
 remotes::install_github("uribo/jpndistrict", upgrade = "never")
 remotes::install_github("ropensci/rnaturalearthhires", upgrade = "never")
